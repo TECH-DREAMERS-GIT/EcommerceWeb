6ebf1c451290ed740159f4fb1ec03abf9966274b
Alex Omondi Otieno <123232209+TECH-DREAMERS-GIT@users.noreply.github.com>
2024-04-30 10:03:20 +0300

    Add files via upload

--- page 1/script.js
+++ page 1/script.js
@@ -1,19 +1,21 @@
-// Select relevant HTML elements
-const filterButtons = document.querySelectorAll("#filter-buttons button");
-const filterableCards = document.querySelectorAll("#filterable-cards .card");
-
-// Function to filter cards based on filter buttons
-const filterCards = (e) => {
-    document.querySelector("#filter-buttons .active").classList.remove("active");
-    e.target.classList.add("active");
-
-    filterableCards.forEach(card => {
-        // show the card if it matches the clicked filter or show all cards if "all" filter is clicked
-        if(card.dataset.name === e.target.dataset.filter || e.target.dataset.filter === "all") {
-            return card.classList.replace("hide", "show");
-        }
-        card.classList.add("hide");
-    });
-}
-
-filterButtons.forEach(button => button.addEventListener("click", filterCards));
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
